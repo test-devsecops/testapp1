e6dde89f9d899df65f04ee8060c96c8ac493e9dd
zh-lim <zhlim1991@gmail.com>
2025-04-21 15:39:22 +0800

    Update checkmarx_group_onboard.py to read env variables

--- .github/scripts/checkmarx_group_onboard.py
+++ .github/scripts/checkmarx_group_onboard.py
@@ -1,5 +1,9 @@
-def createGroups():
-  print("Creating Groups on Checkmarx...")
+import os
+
+def createGroups(tenant_name):
+  print(f"Creating Groups on Checkmarx tenant {tenant_name}...")
 
 if __name__ == "__main__":
-  createGroups()
+  tenant_name = os.environ['CX_TENANT_NAME']
+  
+  createGroups(teanant_name)
